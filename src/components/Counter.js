380d3fbe4674cb9525a9f7c9d9697f059ec75e8a
Hector <hectoraltamira1ai@gmail.com>
2025-03-06 14:44:58 -0800

    initial commit

---
+++ src/components/Counter.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '../redux/actions';
+
+const Counter = () => {
+    const count = useSelector(state => state);
+    const dispatch = useDispatch();
+
+    console.log('Rendering Counter component with count:', count);
+
+    return (
+        <div>
+            <h1>Main Counter Component</h1>
+            <p>Count: {count}</p>
+            <button className="increment" onClick={() => dispatch(increment())}>Increment</button>
+            <button className="decrement" onClick={() => dispatch(decrement())}>Decrement</button>
+        </div>
+    );
+};
+
+export default Counter;
